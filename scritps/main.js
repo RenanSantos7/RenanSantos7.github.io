1a2f472cbd4a5507a7c6e949247f7ab6e8018fc1
RenanSantos7 <36091929+RenanSantos7@users.noreply.github.com>
2023-04-07 12:59:41 -0300

    Initial commit

---
+++ scritps/main.js
@@ -0,0 +1,55 @@
+// % SAUDAÇÃO
+const nascimento = new Date(1990, 02, 08);
+const hoje = new Date(2023, 01, 01);
+const idade = hoje.getFullYear() - nascimento.getFullYear();
+const spanIdade = document.getElementById('idade');
+const spanSaudacao = document.getElementById('saudacao');
+
+spanIdade.textContent = idade;
+
+var saudacao = "";
+if (hoje.getHours < 12) {
+    var saudacao = "Bom dia";
+} else if (12 <= hoje.getHours() < 18) {
+    var saudacao = "Boa tarde";
+} else {
+    var saudacao = "Boa noite"
+}
+
+spanSaudacao.textContent = saudacao;
+
+// % CARROSSEL
+const controles = document.querySelectorAll('.controle');
+const projetos = document.querySelectorAll('.projeto');
+const numProjetos = projetos.length;
+let projetoExibido = 0;
+
+controles.forEach(controle => {
+    controle.addEventListener('click',function(){
+        const isLeft = controle.classList.contains('arrow-left');
+        if(isLeft) {
+            projetoExibido -= 1;
+        } else {
+            projetoExibido += 1;
+        }
+
+        if (projetoExibido >= numProjetos) {
+            projetoExibido = 0;
+        }
+
+        if (projetoExibido < 0) {
+            projetoExibido = numProjetos -1;
+        }
+
+        projetos.forEach(projeto => {
+            projeto.classList.remove('projeto-exibido');
+        });
+
+        projetos[projetoExibido].scrollIntoView({
+            inline: "center",
+            behavior: "smooth"
+        });
+
+        projetos[projetoExibido].classList.add('projeto-exibido');
+    });
+});
