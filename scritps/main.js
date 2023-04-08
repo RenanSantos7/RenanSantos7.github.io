2fd3012f6cdbf997fa9230837abf4e4014a11596
RenanSantos7 <36091929+RenanSantos7@users.noreply.github.com>
2023-04-08 18:19:59 -0300

    Mais frases

--- scritps/main.js
+++ scritps/main.js
@@ -37,7 +37,7 @@ controles.forEach(controle => {
         }
 
         if (projetoExibido < 0) {
-            projetoExibido = numProjetos -1;
+            projetoExibido = numProjetos - 1;
         }
 
         projetos.forEach(projeto => {
@@ -62,7 +62,14 @@ const frases = [
     "I never in my wildest dreams would have predicted the evolution of the Internet and I never would've predicted the degree to which corporate influence over the Internet has changed its character over time.",
     "You've got to start with the customer experience and work back toward the technology — not the other way around",
     "Professionalism has no place in art, and hacking is art. Software Engineering might be science; but that's not what I do. I'm a hacker, not an engineer.",
-    "Em qualquer era da historia todo dia sai alguém procurando o trouxa da vez. Apenas se certifique de que o trouxa não é você"
+    "Em qualquer era da historia todo dia sai alguém procurando o trouxa da vez. Apenas se certifique de que o trouxa não é você",
+    "Não é a linguagem de programação que define o programador, mas sim sua lógica.",
+    "Só por que alguma coisa não faz o que você planejou que ela fizesse não quer dizer que ela seja inútil.",
+    "Programadores e artistas são os únicos profissionais que tem como hobby a própria profissão.",
+    "Saber não é o bastante, devemos aplicar. Disposição não é o suficiente, devemos fazer.",
+    "Nossos fracassos, às vezes, são mais frutíferos do que os êxitos.",
+    "Não se preocupe se não funcionar corretamente. Se tudo acontecesse, você ficaria sem emprego.",
+    "Faça como um programador. Quando tudo está errado e confuso, apague tudo e recomece do zero."
 ];
 
 const autores = [
@@ -71,7 +78,14 @@ const autores = [
     "L Peter Deutsch",
     "Steve Jobs",
     "Jamie Zawinski",
-    "Fabio Akita"
+    "Fabio Akita",
+    "David Ribeiro Guilherme",
+    "Thomas Edison",
+    "Rafael Lain",
+    "Johann Wolfgang von Goethe",
+    "Henry Ford",
+    "Lei de Mosher",
+    "Orlando Cordeiro"
 ];
 
 // const frases2 = {
