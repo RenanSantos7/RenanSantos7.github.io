1c187a28b8615725381cf130396821671cb78f5b
RenanSantos7 <renansantos7@proton.me>
2023-12-10 10:31:46 -0300

    menuMobile concluído

--- scritps/main.js
+++ scritps/main.js
@@ -5,11 +5,4 @@ import menuMobile from './menuMobile.js'
 animaMenu()
 document.addEventListener('load', mudaFrase());
 
-// Cria uma media query que verifica se a largura da tela é menor que 700 pixels
-var mediaQuery = window.matchMedia("(max-width: 700px)")
-
-// Adiciona um evento que chama a função menuMobile() sempre que a media query mudar
-mediaQuery.addEventListener(menuMobile)
-
-// Chama a função menuMobile() pela primeira vez
 menuMobile()
