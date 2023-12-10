5da34e87109935fadf19189615aed4a7668d04bd
RenanSantos7 <renansantos7@proton.me>
2023-12-10 09:42:16 -0300

    melhorias de acessibilidade

--- scritps/componentes/cabecalho.js
+++ scritps/componentes/cabecalho.js
@@ -6,11 +6,11 @@ export default function cabecalho() {
         <a href="../index.html">Início</a>
         <div class="links">
             <a href="../index.html#sobre-mim">Sobre mim</a>
-            <a href="https://www.linkedin.com/in/renan-santos-7332b5255/" id="LinkedIn" title="LinkedIn">
-                <i class="fa-brands fa-linkedin"></i>
-            </a>
-            <a id="github" href="https://github.com/RenanSantos7" title="Github">
-                <i class="fa-brands fa-github"></i>
+            <a target="_blank" rel="noopener" aria-label="LinkedIn" id="LinkedIn" href="https://www.linkedin.com/in/renan-santos-7332b5255/">
+                <i class="fa-brands fa-linkedin" aria-hidden="true"></i>
+            </a>                
+            <a target="_blank" rel="noopener" id="github" aria-label="Github" href="https://github.com/RenanSantos7">
+                <i class="fa-brands fa-github"  aria-hidden="true"></i>
             </a>
         </div>
     `
