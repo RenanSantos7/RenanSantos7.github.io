9b7b861f296032211a06a5ee824d8e9618be9660
RenanSantos7 <renansantos7@proton.me>
2023-12-19 12:13:28 -0300

    corrigindo bug na página de projetos

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-import animaMenu from './animMenu.js'
 import mudaFrase from './mudafrase.js'
 import menuMobile from './menuMobile.js'
 import listaProjetos from './listaProjetos.js'
+import animaMenu from './animMenu.js'
 
 // animação que diminui o menu conforme o scroll
 animaMenu()
--- scripts/pagProjetos.js
+++ scripts/pagProjetos.js
@@ -2,9 +2,6 @@ import projetosDados from "./projetosDados.js";
 import cabecalho from "./componentes/cabecalho.js";
 import menuMobile from './menuMobile.js'
 
-cabecalho()
-menuMobile()
-
 var pagina = parseInt(localStorage.getItem('projetoID'))
 
 montaPagina(projetosDados[pagina])
@@ -12,15 +9,18 @@ montaPagina(projetosDados[pagina])
 function montaPagina(projeto) {
     const header = document.querySelector(".projeto__header")
     const projetoDescricao = document.querySelector("#projeto__descrição")
-
+    
     header.innerHTML = `
-        <div id="titulo">
-            <h1>${projeto.nome}</h1>
-            <a href="${projeto.link}">Visitar projeto</a>
-        </div>
-        <figure>
-            <img src="${projeto.img}" alt="Vade Mecum Tela Principal">
-        </figure>
+    <div id="titulo">
+    <h1>${projeto.nome}</h1>
+    <a href="${projeto.link}">Visitar projeto</a>
+    </div>
+    <figure>
+    <img src="${projeto.img}" alt="Vade Mecum Tela Principal">
+    </figure>
     `
     projetoDescricao.innerHTML = projeto.texto
 }
+
+cabecalho()
+menuMobile()
