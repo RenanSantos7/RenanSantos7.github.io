546ab3492a59c96a18285ebdef2680bb0016d8ad
RenanSantos7 <renansantos7@proton.me>
2023-12-20 21:36:36 -0300

    Animação do menu feita com css

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,6 @@
 import mudaFrase from './mudafrase.js'
 import menuMobile from './menuMobile.js'
 import listaProjetos from './listaProjetos.js'
-import animaMenu from './animMenu.js'
-
-// animação que diminui o menu conforme o scroll
-animaMenu()
 
 // cria os cards dos projetos
 listaProjetos()
