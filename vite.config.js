8aba784637ac623eac36d0bceaed46299c792329
Renan Santos <renansantos7@proton.me>
2024-01-25 18:41:40 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: "@", replacement: "/src" }]
   },
 })
