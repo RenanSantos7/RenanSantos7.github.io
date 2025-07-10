8fce7d03b13fd9757b057f89c49d3d82aac6b651
RenanSantos7 <renansantos7@proton.me>
2025-07-10 08:57:42 -0300

    fix: :bug: solucionado problema de restauração de rolagem ao abrir nova página.

--- src/pages/Base.tsx
+++ src/pages/Base.tsx
@@ -1,14 +1,20 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
 import Rodape from '../components/PageFooter';
 import RollToUp from '../components/RollToUp';
+import { useLayoutEffect } from 'react';
 
 export default function Base() {
+	const location = useLocation();
+
+	useLayoutEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+	}, [location.pathname]);
+	
 	return (
 		<>
 			<Outlet />
 			<Rodape />
 			<RollToUp />
-			<ScrollRestoration />
 		</>
 	);
 }
